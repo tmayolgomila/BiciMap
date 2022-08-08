bd0bf401a763942890647706613fcf536c2ea426
naxinga <ngarconf@hotmail.com>
2022-08-08 11:39:04 +0000

    iniciando maps

---
+++ src/front/js/component/maps.js
@@ -0,0 +1,10 @@
+import React from "react";
+import googleMapReact from "google-map-react";
+
+export const Maps = () => {
+    const { store, actions } = useContext(Context);
+
+    return (
+        <></>
+    );
+};
