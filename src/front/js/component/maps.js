b67ce3d27377c485463587924c28e66ce33f3f58
naxinga <ngarconf@hotmail.com>
2022-08-08 12:33:59 +0000

    Implementado pero no se ve

--- src/front/js/component/maps.js
+++ src/front/js/component/maps.js
@@ -1,10 +1,18 @@
 import React from "react";
-import googleMapReact from "google-map-react";
+import GoogleMaps from "simple-react-google-maps"
 
 export const Maps = () => {
-    const { store, actions } = useContext(Context);
 
     return (
-        <></>
+        <div className="container">
+            <GoogleMaps
+                apiKey={"AIzaSyDLMSEUsNXiJ9AYqkgSt8ADPEnW8jBNGm8"}
+                style={{height: "400px", width: "100%"}}
+                zoom={10}
+                center={{lat: 37.4224764, lng: -122.0842499}}
+                markers={{lat: 37.4224764, lng: -122.0842499}} //optional
+            />
+                   
+        </div>
     );
 };
