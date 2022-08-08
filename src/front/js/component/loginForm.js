e6141436c9600b978987fd85ab7c06e9ea1c8b5f
tmayolgomila <tmayolgomila@gmail.com>
2022-08-08 17:05:08 +0000

    limpiando appcontext y flux

---
+++ src/front/js/component/loginForm.js
@@ -0,0 +1,37 @@
+import { useNavigate } from "react-router-dom";
+import React,{ useContext, useState } from 'react';
+import { Context } from "../store/appContext";
+
+
+
+
+function LoginForm() {
+  const{store, actions}= useContext(Context)
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const navigate = useNavigate()
+  
+  return (
+    <>
+  <div className="mb-3">
+    <label>Email</label>
+    <input onChange={(e)=>setUsername(e.target.value)} type="text" placeholder="Enter email" />
+  </div>
+  <div className="mb-3">
+        <label>Password</label>
+        <input onChange={(e)=>setPassword(e.target.value)} type="password" placeholder="Password" />
+      </div>
+  <button variant="primary" type="submit" onClick={()=>{
+    if(email === "" || password === ""){
+      alert("campos vacios, rellenar")
+    }else{
+      actions.login(username,password, navigate)
+    };
+  }}>
+    Login
+  </button>
+</>
+  );
+}
+
+export default LoginForm;
