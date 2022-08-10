135de3dffc35bd876806e33cb853887272244c0a
tmayolgomila <tmayolgomila@gmail.com>
2022-08-10 18:15:53 +0000

    arreglando desarrollo

--- src/front/js/component/loginForm.js
+++ src/front/js/component/loginForm.js
@@ -8,21 +8,20 @@ import "../../styles/formularioLogin.css";
 
 function LoginForm() {
   const{store, actions}= useContext(Context)
-<<<<<<< HEAD
 
-  const [username, setUsername] = useState("")
+
+
 
   const [email, setEmail] = useState("")
 
-=======
-  const [username, setUsername] = useState("")
-  const [email, setEmail] = useState("")
->>>>>>> signup
+
+
+
   const [password, setPassword] = useState("")
   const navigate = useNavigate()
   
   return (
-<<<<<<< HEAD
+
     <div className="container">
 
 
@@ -48,31 +47,7 @@ function LoginForm() {
       Login
     </button>
   </div>
-  
-=======
-    <>
-  <div className="mb-3">
-    <label>Email</label>
-    <input onChange={(e)=>setUsername(e.target.value)} type="text" placeholder="Enter email" />
-    <label>email</label>
-    <input onChange={(e)=>setEmail(e.target.value)} type="text" placeholder="Enter email" />
-  </div>
-  <div className="mb-3">
-        <label>Password</label>
-        <input onChange={(e)=>setPassword(e.target.value)} type="password" placeholder="Password" />
-      </div>
-  <button variant="primary" type="submit" onClick={()=>{
-    if(email === "" || password === ""){
-      alert("campos vacios, rellenar")
-    }else{
-      actions.login(username,password, navigate)
-      actions.login(email,password, navigate)
-    };
-  }}>
-    Login
-  </button>
-</>
->>>>>>> signup
+
   );
 }
 
