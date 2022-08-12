a683d3f14eed497690de66fc57b8427d1aebc304
naxinga <ngarconf@hotmail.com>
2022-08-12 16:55:23 +0000

    Makers añadidos

--- src/front/js/component/maps.js
+++ src/front/js/component/maps.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 import GoogleMaps from "simple-react-google-maps"
 
 
@@ -14,11 +13,16 @@ export const Maps = () => {
 
                 apiKey={"AIzaSyCsIahTZGVsJhVL2YWTddAmieOAlvv4Ibk"}
                 style={{height: "700px", width: "100%"}}
-                zoom={10}
+                zoom={13}
                 center={{lat: 37.3890924, lng: -5.9844589}}
 
 
-                markers={{lat: 37.3890924, lng: -5.9844589}}
+                markers={[
+                    {lat: 37.376867, lng: -5.987328},
+                    {lat: 37.391200, lng: -5.975360},
+                    {lat: 37.383284, lng: -5.970637},
+                    {lat: 37.399116, lng: -5.993841}
+                ]}
             />
                    
         </div>
