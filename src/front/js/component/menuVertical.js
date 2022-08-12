1577489b63130d92b5b1bd2e0b16722157299cd2
tmayolgomila <tmayolgomila@gmail.com>
2022-08-12 19:31:47 +0000

    menu vertical

--- src/front/js/component/menuVertical.js
+++ src/front/js/component/menuVertical.js
@@ -8,6 +8,14 @@ export const MenuVertical = () => {
 	return (
 
 <>
+<div className="vertical-menu">
+  <p href="#" class="active">Home</p>
+  <p href="#">Link 1</p>
+  <p href="#">Link 2</p>
+  <p href="#">Link 3</p>
+  <p href="#">Link 4</p>
+</div>
+
 </>
 		
     );
