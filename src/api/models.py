a2e76e37bd17bb5216c8352cd4ac8221d3842cf3
tmayolgomila <tmayolgomila@gmail.com>
2022-08-19 17:03:45 +0000

    cambiando a rama bike

--- src/api/models.py
+++ src/api/models.py
@@ -23,3 +23,23 @@ class User(db.Model):
             # do not serialize the password, its a security breach
         }
     
+class Bike(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tipo = db.Column(db.String(120), unique=False, nullable=False)
+    Foto = db.Column(db.String(120), unique=False, nullable=False)
+    nombre = db.Column(db.String(80), unique=False, nullable=False)
+    apellidos = db.Column(db.String(120), unique=False, nullable=False)
+    telefono = db.Column(db.Integer, unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "nombre": self.nombre,
+            "apellidos": self.apellidos, 
+            "telefono": self.telefono,
+            # do not serialize the password, its a security breach
+        }
