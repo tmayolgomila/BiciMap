a62480e1f6dc072e29b92abbc065a628da23efbd
tmayolgomila <tmayolgomila@gmail.com>
2022-09-01 15:22:05 +0000

    Pagos con Paypal y tarjeta

--- src/front/js/component/Pagar.js
+++ src/front/js/component/Pagar.js
@@ -1,11 +1,18 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom"
 
 const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
 
 export default function Pago(){
   const [price, setPrice] = useState(0.30);
-  const [opcion, setOpcion] = useState(0)
+  const [opcion, setOpcion] = useState(0.30)
+
+  useEffect(()=> {
+    if (opcion != "other"){
+      setPrice(opcion);
+    }
+  }, [opcion])
+
   const createOrder = (data, actions) =>{
     return actions.order.create({
       purchase_units: [
@@ -18,19 +25,24 @@ export default function Pago(){
     });
   }
   const onApprove = (data, actions) => {
-    return actions.order.capture();
+    return actions.order.capture(alert("Pago realizado con éxito"));
   }
-  function handleChange(e){
+  const handleChange = (e)=>{
     setPrice(e.target.value);
   }
+  const handleCambio = (e)=>{
+    setOpcion(e.target.value);
+  }
+
 
   return(
     <>
     <h2>A pagar {price} $</h2>
-    <select>
-      <opcion></opcion>
+    <select value={opcion} onChange={handleCambio}>
+      <option value="0.3">Alquila</option>
+      <option value="1000">Compra(Introducir cantidad acordada)</option>
     </select>
-    <input type="text" onChange={handleChange} value={price}></input>
+    {opcion ==="1000" && (<input type="text" onChange={handleChange} value={price} placeholder="Introducir cantidad acordada" size={32} ></input>)}
     <PayPalButton
         createOrder={(data, actions) => createOrder(data, actions)}
         onApprove={(data, actions) => onApprove(data, actions)}
