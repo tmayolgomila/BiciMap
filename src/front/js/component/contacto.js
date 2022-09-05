714e1a05f93f743b01ca52696edd244ac97dc76c
tmayolgomila <tmayolgomila@gmail.com>
2022-09-05 12:55:51 +0000

    añado modal

--- src/front/js/component/contacto.js
+++ src/front/js/component/contacto.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import "../../styles/formularioContacto.css";
+import Modal from "react-modal";
+
 
 export default function Contacto() {
+  const [isOpen, setIsOpen] = useState(false);
+  function toggleModal() {
+    setIsOpen(!isOpen);
+  }
+  function cerrar (){
+    setIsOpen(!isOpen);
+}
+  const customStyles = {
+    overlay: {
+      position: "fixed",
+      backgroundColor: "rgba(15, 26, 32, 0.75)",
+    },
+    content: {
+      top: "25%",
+      left: "50%",
+      width: "350px",
+      height: "150px",
+      transform: "translate(-50%, -50%)",
+      backgroundColor: "#0F1A20",
+      color: "white",
+      borderColor: "#2FBF71",
+    },
+  };
+  
   return (
     <div>
       <h1>Contacta con nosotros</h1>
@@ -36,11 +62,30 @@ export default function Contacto() {
             /></p>
           </div>
           <input type="submit" value="Enviar" className="botonContacto" ></input>
+
           <input
             type="hidden"
             name="_next"
-            value="https://3000-tmayolgomila-bicimap-hv3l6nn547y.ws-eu63.gitpod.io"
+            value="https://3000-tmayolgomila-bicimap-auiu7gj70uc.ws-eu63.gitpod.io"
+            onClick={toggleModal}
           ></input>
+           <Modal
+        isOpen={isOpen}
+        onRequestClose={toggleModal}
+        contentLabel="My dialog"
+        style={customStyles}
+        ariaHideApp={false}
+      >
+        <div>
+          
+            <p className="ter">¡Mensaje realizado con éxito!</p>
+          
+        </div>
+        <button id="botonModal" onClick={cerrar}>
+          Close modal
+        </button>
+      </Modal>
+          
           <input type="hidden" name="_captcha" value="false"></input>
         </form>
       </div>
