979f291cf6ee4a8dc72586a16cbddd6d3f6c57ef
tmayolgomila <tmayolgomila@gmail.com>
2022-10-17 14:53:02 +0000

    cambio fotos

--- src/front/js/component/carrousel.js
+++ src/front/js/component/carrousel.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "../../styles/carrousel.css";
-
+import bicimap1 from ".//bicimap1-2.jpg";
+ import bicimap2 from ".//bicimap2.jpg";
+ import bicimap3 from ".//bicimap3.jpg";
+ import bicimap4 from ".//bicimap4.jpg";  
 
 export default function Gallery() {
   return (
     <div className="cajaCarrousel" >
     <div id="slider">
       <figure>
-        <img src="https://images-ext-1.discordapp.net/external/-yF6-ZcZywFSNrDgZoMGPTS0nf0ifVVkjH3WFI6kNh4/%3Fcs%3Dsrgb%26dl%3Dpexels-jahangeer-bm-1619299.jpg%26fm%3Djpg/https/images.pexels.com/photos/1619299/pexels-photo-1619299.jpeg?width=1014&height=676" alt="" />
-        <img src="https://images-ext-1.discordapp.net/external/UQalDA0S4SH5OXQ49dgbcciLVChx-ZYN4roxFiNfII8/%3Fcs%3Dsrgb%26dl%3Dpexels-dominika-roseclay-1239460.jpg%26fm%3Djpg/https/images.pexels.com/photos/1239460/pexels-photo-1239460.jpeg?width=1047&height=676" alt="" />
-        <img src="https://images-ext-1.discordapp.net/external/T8H1iQQaK4Pxo-vJyL5P2DkWTiL77ZsnUVSj_N7tKLQ/%3Fcs%3Dsrgb%26dl%3Dpexels-maria-orlova-4947124.jpg%26fm%3Djpg/https/images.pexels.com/photos/4947124/pexels-photo-4947124.jpeg?width=1014&height=676" alt="" />
-        <img src="https://images-ext-1.discordapp.net/external/T8H1iQQaK4Pxo-vJyL5P2DkWTiL77ZsnUVSj_N7tKLQ/%3Fcs%3Dsrgb%26dl%3Dpexels-maria-orlova-4947124.jpg%26fm%3Djpg/https/images.pexels.com/photos/4947124/pexels-photo-4947124.jpeg?width=1014&height=676" alt="" />
+        <img src={bicimap1} alt="" />
+        <img src={bicimap2} alt="" />
+        <img src={bicimap3} alt="" />
+        <img src={bicimap4} alt="" />
 
       </figure>
     </div>
